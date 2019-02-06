41f465f9bb599fa2ef7ac4691b4d8f575d444d29
Chris Kennedy <chrisken@gmail.com>
2019-02-05 19:09:49 -0800

    outcome_family: fix function argument bug

--- R/outcome-family.R
+++ R/outcome-family.R
@@ -1,10 +1,14 @@
 #' Detect outcome family based on the outcome data vector
+#'
+#' @param data tbd
+#' @param outcome_field tbd
+#'
 #' @export
 outcome_family = function(data, outcome_field) {
 
-  unique_vals = unique(data[[outcome]])
+  unique_vals = unique(data[[outcome_field]])
   if (length(unique_vals) == 2L &&
-      length(setdiff(unique(data[[outcome]]), c(0, 1))) == 0L) {
+      length(setdiff(unique(data[[outcome_field]]), c(0, 1))) == 0L) {
     family = "binomial"
   } else {
     family = "gaussian"
