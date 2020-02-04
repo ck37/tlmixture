b8fcdfb8f5babd8d2440e4046aebe29b57b2a87d
Chris Kennedy <chrisken@gmail.com>
2020-02-04 09:05:19 -0800

    display mixture improvements

--- R/display-mixture.R
+++ R/display-mixture.R
@@ -4,13 +4,19 @@
 #' @param digits tbd
 #'
 #' @export
-display_mixture = function(analysis, digits = 3) {
+display_mixture =
+  function(analysis, digits = 3,
+           caption = "Mixture analysis",
+           label = NULL,
+           booktabs = TRUE) {
 
   # Save current value of this option.
   old_value = getOption("knitr.kable.NA")
   # We need to do this to print NA's as empty strings.
   options(knitr.kable.NA = '')
 
+
+  # TODO: add adjusted outcome
   tab2 = analysis$tab2
   rownames(tab2)[1:2] = c("Mixture", "Outcome")
 
@@ -19,12 +25,16 @@ display_mixture = function(analysis, digits = 3) {
   # Set weight column to NA.
   # tab3[, 1] = NA
 
-  kab_tab = kableExtra::kable(rbind(tab2, tab3), digits = digits) %>%
+  # TODO: labe mixture quantiles as the columns.
+
+  kab_tab = kableExtra::kable(rbind(tab2, tab3), digits = digits, booktabs = booktabs,
+                              caption = caption, label = label) %>%
           kableExtra::kable_styling(bootstrap_options = c("striped", "hover"),
                         full_width = FALSE) %>%
-          kableExtra::pack_rows("Mixture", 1, 2) %>%
+          kableExtra::add_header_above(c(" " = 1, "Mean by mixture quantile" = ncol(tab2))) %>%
+          #kableExtra::pack_rows("Mixture", 1, 2) %>%
           kableExtra::pack_rows("Exposures", 3, nrow(tab2)) %>%
-          kableExtra::pack_rows("Adjustment variables", nrow(tab2) + 1, nrow(tab2) + nrow(tab3))
+          kableExtra::pack_rows("Covariates", nrow(tab2) + 1, nrow(tab2) + nrow(tab3))
 
 
   # Restore old option value if we had to modify it.
